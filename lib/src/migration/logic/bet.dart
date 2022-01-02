993fcffc80a9ed81eb0e6ddf08b3467078d78763
gierr7976 <gierr7976@gmail.com>
2022-01-02 21:09:26 +0300

    ref: Исключено дублирование кода

--- lib/src/migration/logic/bet.dart
+++ lib/src/migration/logic/bet.dart
@@ -9,19 +9,9 @@ class Bet {
 
   bool get folded => _folded;
 
-  Bet.name(this.author, this._amount, this._folded);
+  Bet(this.author, this._amount, this._folded);
 
-  bool call(Bet another) {
-    if (folded) return false;
-
-    int toAdd = another.amount - _amount;
-    bool charged = author.charge(toAdd);
-    if (charged == true) {
-      _amount += toAdd;
-      return true;
-    } else
-      return false;
-  }
+  bool call(Bet another) => bet(another.amount);
 
   bool bet(int amount) {
     if (folded) return false;
