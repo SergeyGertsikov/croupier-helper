453c54de273500b72fdad8f148b9cc141fe420e8
gierr7976 <gierr7976@gmail.com>
2022-01-02 23:11:07 +0300

    feature: Добавлен TurnHelper

--- lib/src/migration/logic/abstract/street.dart
+++ lib/src/migration/logic/abstract/street.dart
@@ -1,5 +1,9 @@
+import 'package:meta/meta.dart';
 import 'package:pockergui1/src/migration/logic/abstract/player_info.dart';
+import 'package:pockergui1/src/migration/logic/bet.dart';
 import 'package:pockergui1/src/migration/logic/player.dart';
+
+typedef StreetFinishCallback = void Function(int bank);
 
 /// Этап раунда.
 ///
@@ -14,14 +18,27 @@ import 'package:pockergui1/src/migration/logic/player.dart';
 /// }
 /// ```
 abstract class Street<T> {
+  static List<Bet> betsFromPlayers(List<Player> players) => [
+        for (Player player in players) Bet(player),
+      ];
+
   /// Тип-имя улицы (flop, turn, etc).
   final T type;
+
+  final StreetFinishCallback onFinish;
+
+  @protected
+  final List<Bet> bets;
 
   /// Конструктор по умолчанию.
   ///
   /// <br>
   /// [players] — список участников этапа.
-  Street(this.type, List<Player> players);
+  Street({
+    required this.type,
+    required this.onFinish,
+    required List<Player> players,
+  }) : bets = betsFromPlayers(players);
 
   /// Информация об игроках и ставках для отображения.
   List<PlayerRoundInfo> get roundInfo;
@@ -43,4 +60,52 @@ abstract class Street<T> {
 
   /// Выполняет фолд от имени текущего игрока.
   void fold();
+
+  @protected
+  void commitBet();
+}
+
+mixin TurnHelper<T> on Street<T> {
+  late final List<Player> _pending;
+
+  late final List<Player> _finished;
+
+  late Player _current;
+
+  Bet get current => bets.firstWhere((bet) => bet.author == _current);
+
+  void initTurnHelper({
+    required List<Player> players,
+    required Player starter,
+  }) {
+    assert(
+      players.contains(starter),
+      'Starter player shall participate street!',
+    );
+
+    _pending = List.of(players);
+    _pending.remove(starter);
+
+    _current = starter;
+
+    _finished = [];
+  }
+
+  void resetTurn() {
+    _pending.addAll(_finished);
+
+    _finished.clear();
+  }
+
+  @override
+  void commitBet(){
+    _finished.add(_current);
+
+    Player next = nextPlayer(_pending);
+    _pending.remove(next);
+
+    _current = next;
+  }
+
+  Player nextPlayer(List<Player> pending);
 }
